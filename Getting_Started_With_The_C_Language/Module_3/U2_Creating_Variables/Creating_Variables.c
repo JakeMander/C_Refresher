c91058ae6987fd2097d618d03301fe6cc26513d0
Jake Mander <Jake@chilli-is.co.uk>
2021-06-08 00:19:40 +0100

    Completed Module 3

---
+++ Getting_Started_With_The_C_Language/Module_3/U2_Creating_Variables/Creating_Variables.c
@@ -0,0 +1,14 @@
+//  Modify the previous "Hello World" example to do something different!
+#include <stdio.h>
+
+int main(void)
+{
+    /* Prompt the user to enter a temperature value in Farenheit. */
+    printf("Please Enter A Temperature In Farenheit:\n");
+
+    //  Variable decleration - Storage for user input - Specify the type and the
+    //  name of the variable. Optionally, an initial value can be provided.
+    float temperatureF = 42.3;
+
+    return 0;
+}
