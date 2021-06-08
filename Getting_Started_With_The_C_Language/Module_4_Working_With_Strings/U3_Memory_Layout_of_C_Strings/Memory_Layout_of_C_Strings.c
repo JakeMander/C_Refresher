47e8f8f8a28b66b4c0196f19a626a016c17e69e8
Jake Mander <Jake@chilli-is.co.uk>
2021-06-08 21:07:54 +0100

    Completion of module 3, start of module 4.

---
+++ Getting_Started_With_The_C_Language/Module_4_Working_With_Strings/U3_Memory_Layout_of_C_Strings/Memory_Layout_of_C_Strings.c
@@ -0,0 +1,17 @@
+/*
+    The following is a discussion !
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    /*  
+        Character array. Char is a C type! In this instance, it is used to tell the C compiler that we are using an array of single
+        characters organised into a single block of memory. 
+         
+        "firstname" is the identifier that represents the name of the string variable.
+     
+        "Jake" is the content of the string. This is a "string literal"!
+    */
+    char firstName[] = "Jake";
+}
